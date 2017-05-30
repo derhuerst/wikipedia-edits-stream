961c32a9226da3d0a6a9e878177c5b29adb8f806
Jannis R <mail@jannisr.de>
2017-05-31 00:02:02 +0200

    implementation :clap:

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 'use strict'
 
-// todo
+const passthrough = require('readable-stream/passthrough')
+const EventSource = require('eventsource')
+
+const endpoint = 'https://stream.wikimedia.org/v2/stream/recentchange'
+
+const edits = () => {
+	// todo: close src if stream gets closed
+	const out = passthrough({objectMode: true})
+
+	const src = new EventSource(endpoint)
+	src.onmessage = (msg) => {
+		try {
+			const data = JSON.parse(msg.data)
+			out.write(data)
+		} catch (err) {
+			out.emit('error', err)
+		}
+	}
+	src.onerror = (err) => out.emit('error', err)
+
+	out.close = () => src.close()
+
+	return out
+}
+
+module.exports = edits
